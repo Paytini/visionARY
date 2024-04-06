37e771c3712c073841c9b4e4f363f8d709825154
dianaPeVera <diana.perez84@uabc.edu.mx>
2024-04-05 18:25:41 -0700

    Backend, rutas

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,16 +11,44 @@ export default {
     },
     async getUserById(req, res) {
         try {
-            return res
-                .status(200)
-                .send(await userService.getUserById(req.params.id));
+            const user = await userService.getUserById(req.params.id);
+            if (!user) {
+                return res.status(404).json({ error: "Usuario no encontrado" });
+            }
+            return res.status(200).send(user); // Envía toda la información del usuario
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    },    
+    async createUser(req, res) {
+        try {
+            return res.status(200).send(await userService.createUser(req.body));
         } catch (err) {
             res.status(500).send(err);
         }
     },
-    async createUser(req, res) {
+    async getUserEmergencyContacts(req, res) {
         try {
-            return res.status(200).send(await userService.createUser(req.body));
+            // Obtener el usuario por su ID
+            const user = await userService.getUserById(req.params.id);
+
+            // Verificar si el usuario existe
+            if (!user) {
+                return res.status(404).json({ error: "Usuario no encontrado" });
+            }
+            
+
+            // Obtener los contactos de emergencia del usuario
+            const emergencyContacts = user.emergency_contacts;
+
+            // Extraer nombre y teléfono de cada contacto de emergencia
+            const contactsData = emergencyContacts.map(contact => ({
+                name: contact.name,
+                phone: contact.phone
+            }));
+
+            // Enviar los datos de los contactos de emergencia
+            return res.status(200).send(contactsData);
         } catch (err) {
             res.status(500).send(err);
         }
